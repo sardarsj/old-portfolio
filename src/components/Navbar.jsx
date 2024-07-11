db4446dc1bb9845bcfc0897a282d5d179329270b
sardarsj <sardarsjsingh08@gmail.com>
2024-07-11 16:43:32 +0530

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,41 @@ const Navbar = () => {
         <div className="flex flex-shrink-0 items-center">
           <img className="mx-2 w-10" src={logo} alt="logo" />
         </div>
+
+        <div>
+          <ul className="flex">
+            <li className="p-3 hover:cursor-pointer hover:border-b hover:bg-neutral-700 rounded">
+              About Me
+            </li>
+            <li className="p-3 hover:cursor-pointer hover:border-b hover:bg-neutral-700 rounded">
+              Experience
+            </li>
+            <li className="p-3 hover:cursor-pointer hover:border-b hover:bg-neutral-700 rounded">
+              Projects
+            </li>
+            <li className="p-3 hover:cursor-pointer hover:border-b hover:bg-neutral-500 rounded">
+              Technologies
+            </li>
+          </ul>
+        </div>
+
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-          <FaLinkedin />
-          <FaGithub />
-          <FaSquareXTwitter />
-          <FaInstagram />
+          <a
+            className="hover:scale-125 duration-200"
+            href="https://www.linkedin.com/in/sardarsj/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            className="hover:scale-125 duration-200"
+            href="https://github.com/sardarsj"
+            target="_blank"
+          >
+            <FaGithub />
+          </a>
+          {/* <FaSquareXTwitter /> */}
+          {/* <FaInstagram /> */}
         </div>
       </nav>
     </div>
