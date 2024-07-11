9b5fd9be9ed15358bd0bacb349a940aab697010b
sardarsj <sardarsjsingh08@gmail.com>
2024-07-11 15:39:18 +0530

    added motion on components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div>
-      <nav className="mb-20 flex items-center justify-between py-6">
+      <nav className="mb-5 flex items-center justify-between py-4">
         <div className="flex flex-shrink-0 items-center">
           <img className="mx-2 w-10" src={logo} alt="logo" />
         </div>
