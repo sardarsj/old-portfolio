31ac337f0428bb35ac13bcf2855d2db240ec26d3
sardarsj <sardarsjsingh08@gmail.com>
2024-07-11 14:31:23 +0530

    made new features

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+// import logo from "../assets/sj.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+          <img className="mx-2 w-10" src={logo} alt="logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGithub />
+          <FaSquareXTwitter />
+          <FaInstagram />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
